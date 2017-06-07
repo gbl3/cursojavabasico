c536ca7aad033d04be0b0abce257cf6b3142291d
Gustavo <gbl3@cin.ufpe.br>
2017-06-07 15:16:00 -0300

    Aula 10

---
+++ eclipse/curso-java-basico/src/com/gustavo/cursojava/aula10/Variaveis.java
@@ -0,0 +1,16 @@
+package com.gustavo.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		
+		int idade;
+		String nome;
+		String nomeDoMeuCachorro;
+		int _idade;
+		int $idade;
+		String ano2014;
+		
+	}
+
+}
